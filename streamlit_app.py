6456842a54b2cb9002dea64690ead6b6fe701422
aarun <138357865+send2aarun@users.noreply.github.com>
2023-08-11 13:12:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+Streamlit.text(' Omega 3 & Blueberry Oatmeal')
+Streamlit.text(' Kale, Spinach & Rocket Smoothie')
+Streamlit.text(' Hard-Boiled Free-Range Egg')
