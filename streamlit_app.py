14bd74f467cf444a323d1d59a9cb9a25ee1da664
aarun <138357865+send2aarun@users.noreply.github.com>
2023-08-11 14:03:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.text('                                 --Viki & Suki')
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg 🫣')
 streamlit.text(' 🥑 + 🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
