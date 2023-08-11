3a686be5b95f84c7379e0279d008bb21d9387916
aarun <138357865+send2aarun@users.noreply.github.com>
2023-08-11 14:01:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text(' --Viki Suki')
+streamlit.text('                                 --Viki & Suki')
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
