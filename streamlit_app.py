522a1ac1575878721cf761c61668833c33c9fdec
aarun <138357865+send2aarun@users.noreply.github.com>
2023-08-17 13:17:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,20 +12,20 @@ streamlit.text(' 🥑 + 🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+# my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+# # Let's put a pick list here so they can pick the fruit they want to include
+# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado', 'Strawberries'])
+# fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+# # Display the table on the page.
+# streamlit.dataframe(fruits_to_show)
 
 
 
-#New Section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+# #New Section to display fruityvice api response
+# streamlit.header("Fruityvice Fruit Advice!")
 
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+# # import requests
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# streamlit.text(fruityvice_response)
