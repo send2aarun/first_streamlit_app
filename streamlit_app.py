7a90eded4220da13c90b17a493700190262210c7
aarun <138357865+send2aarun@users.noreply.github.com>
2023-08-17 14:24:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,12 +26,16 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado',
 
 
 # #New Section to display fruityvice api response
-# streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
+
 
 # # import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# streamlit.header("Fruityvice Fruit Advice!")
 # streamlit.text(fruityvice_response.json()) 
 
 # take the json version of the response and normalize it
